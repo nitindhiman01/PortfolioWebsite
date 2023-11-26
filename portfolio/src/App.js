1fc298f589ae8b2ced2da85d116265975640e383
Nitin Dhiman <nitindhiman8773@gmail.com>
2023-11-26 16:55:09 +0530

    Update App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter, HashRouter } from 'react-router-dom';
 import './index.css';
 import PortfolioHomePage from './Components/mainPortfolio';
 import BlogWebsite from './Components/blogWebsite';
@@ -8,13 +8,13 @@ import PageNotFound from './Components/pageNotFound';
 function App() {
   return(
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path='/' index element={<PortfolioHomePage />}></Route>
           <Route path='/blogwebsite' element={<BlogWebsite />}></Route>
           <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
